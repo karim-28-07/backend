f66520b9714bbd54f7601f26994a6dec9a8f9519
karim <karim.mezouar@gmail.com>
2021-04-30 12:31:34 +0200

    Construction d'une route

--- api-countries-react/server.js
+++ api-countries-react/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const  country  = require("./datacountries.js")
+const { reduce } = require("./datacountries.js")
 
 const app = express()
 
@@ -16,6 +17,37 @@ app.get("/country", (req,res) => {
         country
     })
 })
+
+app.get("/country/:name", (req, res)=>{
+
+    const name = req.params.name;
+
+    console.log("name", name)
+
+    const countryName = []
+
+    for(let i = 0; i < country.length; i++){
+        const curCountry = country[i]
+
+        if (curCountry.name === name){
+
+            countryName.push(curCountry)
+        }
+    }
+
+    console.log("countryName", countryName)
+
+    res.json ({
+        countryName
+    })
+
+    
+})
+
+
+
+
+
 
 app.listen(port, () => {
     console.log("Server à l'écoute dans le port 8000");
