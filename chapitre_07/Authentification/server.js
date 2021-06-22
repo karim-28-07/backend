bd085e3db34392e4a77bb7f6019f2e82bfeff08d
karim <karim.mezouar@gmail.com>
2021-06-22 15:21:15 +0200

    route login

--- chapitre_07/Authentification/server.js
+++ chapitre_07/Authentification/server.js
@@ -25,12 +25,40 @@ app.post("/signup", async (req, res) => {
         const user = await userModel.create({passeword, username : req.body.username})
         res.json({user})
            
-        
     } catch (error) {
         res.status(401).json({message: "The user cant not creat"}, error)
     }
-
 })
+
+app.post("/login", async (req, res) => {
+
+    try {
+
+        const user = await userModel.findOne({username})
+        console.log(user)
+
+        const passeword = req.body.passeword
+        console.log(passeword)
+        
+        const result = await bcryptjs.compareSync(passeword, user.passeword)
+        console.log(user.passeword)
+        if(result){
+
+            res.json({
+                message: "the user was connected token"
+            })
+        }else {
+            res.json({
+                message : "there is problem with the username or the passeword"
+            })
+        }
+           
+    } catch (error) {
+        res.status(401).json({message: "The user cant not creat"}, error)
+    }
+})
+
+
 
 app.listen(port, () => {
     console.log("Server is listening at port ", port);
